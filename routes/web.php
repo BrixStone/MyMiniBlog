69a220905fdbc5867593418d4d2659100498d001
KalWarden <uykaiser516@gmail.com>
2026-09-12 13:33:57 +0800

    Change welcome route to home route

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,9 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::view('/', 'welcome')->name('welcome');
+// Route::view('/', 'welcome')->name('welcome');
 
-Route::view('/home', 'home')->name('home');
+Route::view('/', 'home')->name('home');
 
 Route::view('/about', 'about')->name('about');
 
